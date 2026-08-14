ef838ed0428b3324fb3b464f6758668202f20028
André Gonçalves dos Santos <andregoncalvessantos@hotmail.com>
2026-08-14 12:09:12 -0300

    Atualização seguindo os criterios

--- miniprojeto_varejo.py
+++ miniprojeto_varejo.py
@@ -17,7 +17,7 @@ with open('Base Varejo.csv','r', encoding='utf-8') as arquivo:
 
 print(todas_vendas[0])  # Exibe o primeiro registro do arquivo CSV como um dicionário
     
-# --- Etapa 1: Leitura dos dados ---  
+# --- Etapa 2: Leitura dos dados ---  
 df = pd.read_csv('Base Varejo.csv', sep=';', encoding='utf-8')
 
 print(f'Número de linhas: {df.shape[0]}') # df.shape[0] retorna o número de linhas do DataFrame
@@ -26,22 +26,13 @@ print(f'\nColunas e tipos de dados: \n{df.dtypes}')  # df.dtypes exibe as coluna
 print(f'\nPrimeiras linhas: \n{df.head()}') # df.head() Exibe as primeiras linhas do DataFrame
 print(f'\nÚltimas linhas: \n{df.tail()}')  # df.tail() Exibe as últimas linhas do DataFrame
 
-# --- Etapa 2: Limpeza e Tratamento dos Dados ---   
+# --- Etapa 3: Limpeza e Tratamento dos Dados ---   
 
 colunas_validas = [c for c in df.columns if 'Unnamed' not in c]  # Remove colunas com 'unnamed' que estão vazias
 df = df[colunas_validas]  # Mantém apenas as colunas válidas
 
 print(df.columns)   # Exibe as colunas do DataFrame após a limpeza
 print(df.head())  
-
-df['PR_CAT'] = df['PR_CAT'].fillna('Sem Categoria') # Preenche valores nulos na coluna PR_CAT com 'Sem Categoria'
-print(f'Valores nulos na coluna PR_CAT: {df["PR_CAT"].isnull().sum()}')  # Verifica se ainda existem valores nulos na coluna PR_CAT
-
-df = df.drop_duplicates() # Remove linhas duplicadas do DataFrame
-print("Linhas duplicadas removidas!")
-
-df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y') # Converte a coluna 'DATA' para o tipo datetime
-print("Coluna DATA convertida para datetime!")
 
 df = df.rename(
     columns={'DATA': 'Data',
@@ -57,4 +48,15 @@ df = df.rename(
     }
 ) # Renomeia as colunas do DataFrame para nomes mais entendíveis
 print(df.columns)
+
+if df['categoria_produto'].isnull().sum() > 0:
+    df['categoria_produto'] = df['categoria_produto'].fillna('Sem Categoria')
+
+df = df.drop_duplicates() # Remove linhas duplicadas do DataFrame
+print("Linhas duplicadas removidas!")
+
+df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y')
+print("Coluna Data convertida para datetime!")
+
+
 
